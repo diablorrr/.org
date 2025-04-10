470d95bb619de926a8b90b9040de9143187140e1
ysh <1299331829@qq.com>
2025-04-10 16:59:55 +0800

    docs(2025.4.10): 增加笔记

--- example/main.cpp
+++ example/main.cpp
@@ -1,20 +1,31 @@
-#include "two_Sum.h"
+#include <iostream>
+using namespace std;
 
-
-
-
+class Base
+{
+public:
+    Base(int i) :baseI(i){};
+    virtual void print(void){ cout << "调用了虚函数Base::print()"; }
+    virtual void setI(){cout<<"调用了虚函数Base::setI()";}
+    virtual ~Base(){}
+private:
+    int baseI;
+};
 
 
 int main() {
-    std::vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-    std::vector<int> result = twoSum(nums, target);
+    Base b(1000);
+    int * vptrAdree = (int *)(&b);
+    cout << "虚函数指针（vprt）的地址是：\t"<<vptrAdree << endl;
+    typedef void(*Fun)(void);
+    Fun vfunc = (Fun)*( (int *)*(int*)(&b));
+    cout << "第一个虚函数的地址是：" << (int *)*(int*)(&b) << endl;
+    cout << "通过地址，调用虚函数Base::print()：";
+    vfunc();
 
-    if (!result.empty()) {
-        std::cout << "Index: " << result[0] << ", " << result[1] << std::endl;
-    } else {
-        std::cout << "No solution found!" << std::endl;
-    }
-
-    return 0;
+    using VTablePtr = void (​**​)();  // 虚表类型：函数指针数组
+VTablePtr* vptr = reinterpret_cast<VTablePtr*>(&b);  // 获取虚表指针
+Fun firstVFunc = (*vptr)[0];  // 第一个虚函数
+std::cout << "第一个虚函数的地址是：" << reinterpret_cast<void*>(firstVFunc) << std::endl;
+firstVFunc();  // 调用（可能仍需处理this指针）
 }
--- leetcode/code.cpp
+++ leetcode/code.cpp
@@ -1,42 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <algorithm>
 #include <unordered_set>
 #include <unordered_map>
+#include <climits>
 using namespace std;
-
-class Solution {
-public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> res;
-        if (s.size() < p.size()) {
-            return res;
-        }
-        vector<int> counts(26);
-        for (int i = 0; i < p.size(); ++i) {
-            ++counts[p[i] - 'a'];
-        }
-        vector<int> curCounts(26);
-        for (int i = 0; i < p.size() - 1; ++i) {
-            ++curCounts[s[i] - 'a'];
-        }
-        for (int left = 0, right = p.size() - 1; right < s.size(); ++left, ++right) {
-            ++curCounts[s[right] - 'a'];
-            if (counts == curCounts) {
-                res.push_back(left);
-            }
-            --curCounts[s[left] - 'a'];
-        }
-        return res;
-    }
-};
-
-
-int main(){
-    Solution sol;
-    string s = "cbaebabacd", p = "abc";
-    vector<int> resVec = sol.findAnagrams(s, p);
-    for (auto& res:resVec) {
-        cout << res << endl;
-    }
-}
