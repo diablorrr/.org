e8d8fa177299686ec89ca5444e70909bf001d77e
ysh <1299331829@qq.com>
2025-04-18 08:22:20 +0800

    docs(2025.4.18): 增加笔记

--- leetcode/code.cpp
+++ leetcode/code.cpp
@@ -5,4 +5,21 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <climits>
+#include <chrono>
 using namespace std;
+
+class Base {
+    virtual void dummy(){}
+};
+
+class Derived : public Base {
+
+};
+
+int main() {
+    double d = 3.14;
+    cout << static_cast<int>(d);
+
+    const int ci = 19;
+    int* mod = const_cast<int*>(&ci);
+}
